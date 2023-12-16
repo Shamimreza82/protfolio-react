f18299be8c1af4747d6cef1da58141ab21596b14
Shamimreza82 <shamimrezaone@gmail.com>
2023-12-16 23:59:00 +0600

    update footer

--- src/Layout/Banner/Amination.jsx
+++ src/Layout/Banner/Amination.jsx
@@ -8,7 +8,16 @@ const Amination = () => {
         to: { x: 100 },
       })
 
-    
+    return (
+        <animated.div
+          style={{
+            width: 80,
+            height: 80,
+            background: '#ff6d6d',
+            borderRadius: 8,
+          }}
+        />
+      )
 };
 
 export default Amination;
